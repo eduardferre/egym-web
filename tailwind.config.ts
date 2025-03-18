3b263c843f11d7b7ce389bb304ac3352ab70a3eb
eduardferre <eduardferresanchez@gmail.com>
2025-03-18 19:47:13 +0100

    refactor Tailwind CSS integration; update configuration and dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,9 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -15,4 +14,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+}
